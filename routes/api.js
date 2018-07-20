aa282205e18570a031ac23c7a078c92655fe7de7
Benoît Gelineau <bgelineau.dev@gmail.com>
2018-07-20 19:37:54 +0200

    Add IP condition

--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       const stock = req.query.stock;
-      const like = req.query.like;
+      const like = req.query.like ? req.query.like : false;
       let symbol = '';
       
       const getStockOne = async () => {
@@ -37,43 +37,75 @@ module.exports = function (app) {
         const date = api.data['Meta Data']['3. Last Refreshed'];
         const price = api.data['Time Series (1min)'][date]['4. close'].slice(0, -2);
         const count = like ? 1 : 0;
+        const ip = /^(::ffff:)/.test(req.ip) ? req.ip.slice(7) : req.ip;
         
-        mongo.connect( DB, (err, db) => {
+
+        mongo.connect(DB, (err, db) => {
           if (err) {
             console.log('Database error: ' + err);
           } else {
             console.log('Successful database connection');
+
+            const collection = db.collection('stocks');
+
+            // No 'likes' incrementation if IP in ips array
+            collection.findOne(
+              { 
+                stock: stock,
+                ips: { $in: [ip] }
+              },
+              (err, data) => {
+
+                let updates = {};
+                // User registered
+                if (data) {
+                  // No more than 1 like per user
+                  if (like) {
+                    updates = {};
+                  // Remove like and user's ip if checkbox unchecked
+                  } else {
+                    updates = { $unset: { ips: ip }, $inc: { likes: -1 } };
+                  }
+                // User not registered
+                } else {
+                  // Add user's ip and +1 like
+                  if (like) {
+                    updates = {
+                      $setOnInsert: { stock: stock },
+                      $push: { ips: ip },
+                      $inc: { likes: count }
+                    };
+                  // +1 like
+                  } else {
+                    updates = {
+                      $setOnInsert: { stock: stock },
+                      $inc: { likes: count }
+                    };
+                  }
+                }
+
+                collection.findAndModify(
+                  { stock: stock },
+                  {},
+                  updates,
+                  { upsert: true, new: true },
+                  (err, data) => {
+                    if (err) console.log(err);
+
+                    res.json({
+                      stockData: {
+                        stock: data.value.stock,
+                        price: price,
+                        likes: data.value.likes
+                      }
+                    });
+                    db.close();
+                  }
+                );
+              }
+            );
           }
 
-          // db.collection('likes').findAndModify(
-          //   { stock: stock },
-          //   {},
-          //   {
-          //     $setOnInsert: {
-          //       stock: stock,
-          //       likes: count,
-          //       ip: [req.ip]
-          //     },
-          //     $push: {
-          //       ip: req.ip // Push only if doesn't exist already
-          //     },
-          //     $inc: {
-          //       likes: count // Increment only if like is true
-          //     }
-          //   },
-          //   { upsert: true, new: true },
-          //   (err, data) => {
-
-          //     console.log(data);
-          //     res.json({
-          //       stockData: {
-          //         stock: stock,
-          //         price: price,
-          //         likes: count
-          //       }
-          //     });
-          //   }
-          // );
         });
 
       };
